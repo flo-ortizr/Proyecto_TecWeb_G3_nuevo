2207641e72e8ddd3be4b105dd0bf53eb5649e17c
CalebBav234 <calebmbaving@gmail.com>
2025-11-20 09:58:44 -0400

    Feat: created User Repository Interface

---
+++ elearning2/Repositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+using elearning2.Models;
+
+namespace elearning2.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User?> GetByEmailAddress(string email);
+        Task<User?> GetByRefreshToken(string refreshToken); 
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+    }
+}
