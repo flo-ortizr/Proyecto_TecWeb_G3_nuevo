09ed184265eb81296920ae68f311c121ee8de847
CalebBav234 <calebmbaving@gmail.com>
2025-11-20 08:54:41 -0400

    Feat: created User Response

---
+++ elearning2/Models/DTOS/UserResponse.cs
@@ -0,0 +1,9 @@
+namespace elearning2.Models.DTOS
+{
+    public class UserResponse
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+    }
+}
