4443d5cb2cf363e10863124b5ee6efdb43616e3e
CalebBav234 <calebmbaving@gmail.com>
2025-11-24 17:39:50 -0400

    fix: fixed start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-dotnet publish elearning2/elearning2.csproj -c Release -o ./publish
+dotnet publish elearning2/elearning2/elearning2.csproj -c Release -o ./publish
 dotnet ./publish/elearning2.dll
