edbb1df48ee2a00b4277deb240bfdc29f5b74190
CalebBav234 <calebmbaving@gmail.com>
2025-11-25 20:56:28 -0400

    Feat: updated program.cs added connection string from railway

--- elearning2/obj/Debug/net9.0/elearning2.AssemblyInfo.cs
+++ elearning2/obj/Debug/net9.0/elearning2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("elearning2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+31fe4377ce8345498a337d059dab567a918f66e4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8677b11e2cdba0ed32b812162b996cf2eb9b17c3")]
 [assembly: System.Reflection.AssemblyProductAttribute("elearning2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("elearning2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
